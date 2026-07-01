452d762fa4ce3897bbd704e28c8f55c1360a3d71
Keilani Li <knli@ucsd.edu>
2026-06-30 21:20:56 -0700

    listings eda

--- wk2_deliverable.py
+++ wk2_deliverable.py
@@ -54,7 +54,61 @@ for field in core_fields:
 print(len(sold_over90_cols))
 # output: 15
 
-# numeric distrib summary
+# numeric distrib review
 sold_summary = sold[['ClosePrice', 'LivingArea', 'DaysOnMarket']].describe()
 print(sold_summary)
 
+
+
+# save filtered dataset as new csv
+# sold.to_csv('sold.csv', index = False)
+
+
+
+
+# LISTINGS DATASET
+print('Listings dataset shape:', listings.shape)
+print('Listings dataset columns:', listings.columns)
+print('Listings data types:', listings.dtypes)
+print('Listings dataset head:', listings.head())
+
+# check property categories
+print(listings['PropertyType'].unique())
+# output: array(['Residential'], dtype=object)
+
+# validate completeness
+print('Listings dataset null values:', listings.isnull().sum())
+
+# null-count summary table
+listing_null_count = listings.isnull().sum()
+listing_null_percent = listings.isnull().mean() * 100
+
+listing_null_summary = pd.DataFrame({
+    'null count': listing_null_count,
+    'null percent': listing_null_percent
+})
+
+listing_null_summary.sort_values(by = 'null percent', ascending = False).head(10)
+
+# find cols w >90% nulls
+listing_over90 = listing_null_summary[listing_null_summary['null percent'] > 90]
+print('Listings dataset cols w >90% nulls:', listing_over90.shape[0])
+
+# prepare list of cols to flag if they have >90% nulls
+listing_over90_cols = listing_over90.index.tolist()
+print(listing_over90_cols)
+
+# exclude core fields from flagged list if there are any
+for field in core_fields:
+    if field in listing_over90_cols:
+        listing_over90_cols.remove(field)
+
+# check if any flagged cols were removed
+print(len(listing_over90_cols))
+
+# numeric distrib review
+listings_summary = listings[['ClosePrice', 'LivingArea', 'DaysOnMarket']].describe()
+print(listings_summary)
+
+# save filtered dataset as new csv
+# listings.to_csv('listings.csv', index = False)
